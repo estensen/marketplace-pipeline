2999b8394645497f10ececffb955aabd9c46af9d
Håvard Anda Estensen <haavard.ae@gmail.com>
2024-09-23 11:31:24 +0200

    Locally process CSV file with Clickhouse

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # marketplace-pipeline
+
+## Running Clickhouse Locally
+
+### Prerequisites
+- Install clickhouse-loca:
+
+```bash
+curl https://clickhouse.com/ | sh
+```
+
+### Run
+
+```bash
+go run main.go
+```
--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"os/exec"
+)
+
+const csvFile = "sample_data.csv"
 
 func main() {
-	fmt.Println("hello Go")
+	cmd := exec.Command("./clickhouse", "local",
+		"--structure", "date Date, project_id String, transactions_count UInt64, total_volume_usd Float64",
+		"--input-format", "CSV",
+		"--file", csvFile,
+		"--query", "SELECT * FROM file('"+csvFile+"')")
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Printf("Error running clickhouse-local: %v\n", err)
+		fmt.Printf("Output: %s\n", string(output))
+		os.Exit(1)
+	}
+
+	fmt.Printf("Clickhouse-local output: %s\n", string(output))
 }
